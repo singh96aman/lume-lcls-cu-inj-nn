a8abe3573879c2a062998c26bce20e2202b2d0d9
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-30 13:08:35 -0700

    Check in files

--- lume_lcls_cu_inj_nn/__init__.py
+++ lume_lcls_cu_inj_nn/__init__.py
@@ -1,5 +1,13 @@
-from lume_lcls_cu_inj_nn.files import VARIABLE_FILE
+from lume_lcls_cu_inj_nn.files import VARIABLE_FILE, CU_INJ_MAPPING_FILE
 from lume_model.utils import variables_from_yaml
+import pandas as pd
 
 with open(VARIABLE_FILE, "r") as f:
     INPUT_VARIABLES, OUTPUT_VARIABLES = variables_from_yaml(f)
+
+CU_INJ_MAPPING_TABLE = pd.read_csv(CU_INJ_MAPPING_FILE)
+CU_INJ_MAPPING_TABLE.set_index("impact_name")
+
+
+from . import _version
+__version__ = _version.get_versions()['version']
--- lume_lcls_cu_inj_nn/files/__init__.py
+++ lume_lcls_cu_inj_nn/files/__init__.py
@@ -1,3 +1,15 @@
 from pkg_resources import resource_filename
 
 VARIABLE_FILE = resource_filename("lume_lcls_cu_inj_nn.files", "variables.yml")
+
+MODEL_FILE = resource_filename(
+    "lume_lcls_cu_inj_nn.files", "model_1b_AS_f_xy.h5"
+)
+
+CU_INJ_MAPPING_FILE = resource_filename(
+    "lume_lcls_cu_inj_nn.files", "cu_inj_impact.csv"
+)
+
+EPICS_CONFIG_FILE =resource_filename(
+    "lume_lcls_cu_inj_nn.files", "epics_config.yml"
+)
