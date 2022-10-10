acd8f3cfbc197e8e77e8fdf2869bdf9e372f6fba
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-10-10 14:29:30 -0700

    Use scr image

--- lume_lcls_cu_inj_nn/_image.py
+++ lume_lcls_cu_inj_nn/_image.py
@@ -1,3 +1,3 @@
 from lume_lcls_cu_inj_nn import __version__# Stanford Container Registry image
-IMAGE = f"registry.hub.docker.com/jgarrahan/lume-lcls-cu-inj-nn:v{__version__}"
+IMAGE = f"scr.svc.stanford.edu/jgarra/lume-lcls-cu-inj-nn:v{__version__}"
 
