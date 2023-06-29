8aa9b2421b9bba89decc165b5dfab7a0334af44d
singh96aman <amansinghtha@DESKTOP-NCPTG9M>
2023-06-29 12:47:40 -0700

    Updating Jackie to Aman

--- lume_lcls_cu_inj_nn/_image.py
+++ lume_lcls_cu_inj_nn/_image.py
@@ -1,3 +1,3 @@
-from lume_lcls_cu_inj_nn import __version__# Stanford Container Registry image
-IMAGE = f"scr.svc.stanford.edu/jgarra/lume-lcls-cu-inj-nn:v{__version__}"
+from lume_lcls_cu_inj_nn import __version__  # Stanford Container Registry image
 
+IMAGE = f"scr.svc.stanford.edu/singh96aman/lume-lcls-cu-inj-nn:v{__version__}"
