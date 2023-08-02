fc2fde7bd1a9e553e640bc6f96030edd96fac7fd
singh96aman <singh96aman@gmail.com>
2023-08-01 23:36:19 -0700

    Adding Torch Model and Pred Code

--- lume_lcls_cu_inj_nn/flow.py
+++ lume_lcls_cu_inj_nn/flow.py
@@ -20,6 +20,11 @@ from prefect.storage import Module
 from lume_lcls_cu_inj_nn.model import LCLSCuInjNN
 from lume_lcls_cu_inj_nn import INPUT_VARIABLES, CU_INJ_MAPPING_TABLE
 
+import torch
+import matplotlib.pyplot as plt
+
+from lume_model.utils import variables_from_yaml
+from lume_model.torch import LUMEModule, PyTorchModel
 
 @task(log_stdout=True)
 def model_predict(input_variables, settings):
@@ -62,21 +67,31 @@ def preprocessing_task(input_variables):
             )
 
             var.value = scaled_val
-
+    print('Hey2')
+    print(input_variables)
     return input_variables
 
 
 @task(log_stdout=True)
 def format_result(
     input_variables: Dict[str, InputVariable],
-    output_variables: Dict[str, OutputVariable],
+    output_variables,
 ):
-
+    print('Hey 5')
+    output_symbols = ["sigma_x", "sigma_y", "sigma_z", "norm_emit_x", "norm_emit_y"]
     inputs = {var_name: var.value for var_name, var in input_variables.items()}
-    outputs = {var_name: var.value.astype('float64') for var_name, var in output_variables.items()}
+    outputs = {}
+    output_variables = output_variables.tolist()
+
+    
+    for i in range(len(output_variables)):
+        outputs[output_symbols[i]] = output_variables[i]
 
     # convert array to list
-    outputs["x:y"] = outputs["x:y"].tolist()
+    #outputs["x:y"] = outputs["x:y"].tolist()
+
+    print('Final Result')
+    print(Result(inputs=inputs, outputs=outputs))
 
     return Result(inputs=inputs, outputs=outputs)
 
@@ -84,14 +99,47 @@ def format_result(
 @task(log_stdout=True)
 def evaluate(formatted_input_vars):
 
-    model = LCLSCuInjNN()
+    print('Hey 3')
+    all_input_values = []
+    for key in formatted_input_vars:
+        all_input_values.append(formatted_input_vars[key].value)
 
-    return model.evaluate(formatted_input_vars)
+    all_input_values = torch.Tensor([all_input_values])
+    
+    #model = LCLSCuInjNN()
+    
+    input_transformer = torch.load("lume_lcls_cu_inj_nn/model/input_transformer.pt")
+    output_transformer = torch.load("lume_lcls_cu_inj_nn/model/output_transformer.pt")
+    input_variables, output_variables = variables_from_yaml(open("lume_lcls_cu_inj_nn/model/variables.yml"))
+
+    # create lume model
+    lume_model = PyTorchModel(
+        model_file="lume_lcls_cu_inj_nn/model/model.pt",
+        input_variables=input_variables,
+        output_variables=output_variables,
+        input_transformers=[input_transformer],
+        output_transformers=[output_transformer],
+    )
+    lume_module = LUMEModule(
+        model=lume_model,
+        feature_order=lume_model.features,
+        output_order=lume_model.outputs,
+    )
+
+    with torch.no_grad():
+        predictions = lume_module(all_input_values)
+
+    print('Hey 4')
+    print(predictions)
+
+    return predictions
 
 save_db_result_task = SaveDBResult(timeout=30)
 
 with Flow("lume-lcls-cu-inj-nn", storage=Module(__name__)) as flow:
 
+    print('Inside flow')
+    
     # CONFIGURE LUME-SERVICES
     # see https://slaclab.github.io/lume-services/workflows/#configuring-flows-for-use-with-lume-services
     configure = configure_lume_services()
@@ -111,9 +159,12 @@ with Flow("lume-lcls-cu-inj-nn", storage=Module(__name__)) as flow:
         input_variable_parameter_dict, INPUT_VARIABLES
     )
 
+    print('Hey1')
+    print(formatted_input_vars)
+
     # Perform scaling of variables
     processed_input_vars = preprocessing_task(formatted_input_vars)
-
+    
     # RUN EVALUATION
     output_variables = evaluate(processed_input_vars)
 
--- lume_lcls_cu_inj_nn/tests/test_flow.py
+++ lume_lcls_cu_inj_nn/tests/test_flow.py
@@ -3,10 +3,14 @@ from lume_lcls_cu_inj_nn.flow import flow, output_variables
 
 def test_flow_execution():
     # This will require defaults for all parameters
+
+    print('Starting Flow')
+    print(output_variables)
+    
     flow.set_reference_tasks([output_variables])
     flow_run = flow.run(
-        **{
-                        "distgen:r_dist:sigma_xy:value": 0.4130, 
+            **{
+                        "distgen:r_dist:sigma_xy:value": 0.4130,
                         "distgen:total_charge:value": 250.0, 
                         "distgen:t_dist:length:value":7.499772441611215, 
                         "SOL1:solenoid_field_scale": 0.17, 
@@ -15,8 +19,21 @@ def test_flow_execution():
                         "L0A_phase:dtheta0_deg": -8.8997,
                         "L0A_scale:voltage": 70000000.0,
                         "distgen:t_dist:length:value": 7.499772441611215,
-                        "end_mean_z": 4.6147002
+                        "L0B_scale:voltage": 0,
+                        "L0B_phase:dtheta0_deg": 0,
+                        "QA01:b1_gradient": 0,
+                        "QA02:b1_gradient": 0,
+                        "QE01:b1_gradient": 0,
+                        "QE02:b1_gradient": 0,
+                        "QE03:b1_gradient": 0,
+                        "QE04:b1_gradient": 0                     
         }
     )
 
+    print(flow_run)
+
     assert flow_run.is_successful()
+
+print('Before')
+print(output_variables)
+test_flow_execution()
