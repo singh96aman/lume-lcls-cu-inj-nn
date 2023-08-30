9ac350e1714074d4949296818af8c00fa9ba508f
singh96aman <singh96aman@gmail.com>
2023-08-29 22:12:15 -0700

    Loading Dynamic Libs in Flows

--- lume_lcls_cu_inj_nn/_image.py
+++ lume_lcls_cu_inj_nn/_image.py
@@ -1,2 +1,3 @@
 from lume_lcls_cu_inj_nn import __version__# Stanford Container Registry image
-IMAGE = f"scr.svc.stanford.edu/aman96/lume-lcls-cu-inj-nn:v{__version__}"
+#IMAGE = f"scr.svc.stanford.edu/aman96/lume-lcls-cu-inj-nn:v{__version__}"
+IMAGE = f"scr.svc.stanford.edu/aman96/lume-lcls-cu-inj-nn:latest"
